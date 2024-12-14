eec19cc9a3432fbfe26c83d6a7fd003b35a0c118
bhuvaneswar <bhuvaneswarr412@gmail.com.com>
2024-12-14 14:19:22 +0530

    first push to appointment

---
+++ src/main/java/com/drucare/DrucareProjectAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.drucare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrucareProjectAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrucareProjectAppointmentApplication.class, args);
+	}
+
+}
